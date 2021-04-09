03d829bbf2a0d238ecdec77d7560f9dbe4380f2f
Pranav Hegde <pranavhegde06@gmail.com>
2021-04-09 21:17:33 +0530

    finished week9 work

--- Week1/lab1.s
+++ Week1/lab1.s
@@ -1,9 +1,11 @@
-mov r1,#11
-and r3,r1,#1
-cmp r3,#0
-beq label
-mov r2,#0
-swi 0x11
-label:
-mov r2,#1
-swi 0x11
+.text
+    mov r0, #0 ;considered z
+    mov r1, #2 ;considered a
+    mov r2, #3 ;considered b
+
+    mov r3, #4 ;auxillary space 1
+    mov r4, #0 ;auxillary space 2
+
+    and r3, r2, #15
+    mov r4, r1, LSL #2
+    orr r0, r3, r4 
